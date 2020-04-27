f7ce11ce0a8b05b6eec4ee613436ca9dcf5cb313
krystanRamcharan <44042620+krystanRamcharan@users.noreply.github.com>
2020-04-27 12:24:04 -0400

    Add files via upload

--- Enemy.java
+++ Enemy.java
@@ -1,25 +1,23 @@
-// Enemy class
-
 public class Enemy{
 	
-	private int dropThreshold;
-	private int damageThreshold;
-	private int damageRate;
-	private int dropRate;
-	private int damageHp;
-	private String name;
-	private String itemType;
+   public int dropThreshold;
+   public int damageHP;
+   public int damagestam;
+   public int damageThreshold;
+   public String name;
+   public String itemType;
+   public int dropRate;
+   public int damageRate;
+
+
+public void Enemy(){
+
+	dropThreshold, damageThreshold=0;
+	damageHP,damagestam,damageRate=0;
+	dropRate=0;
 	
-	
-	// Methods
-	public boolean dropItem(){}
-	
-	public void inflictDamage(Player player){}
-	
-	
-	
-	// Getters/Setters
-	
-	
-	
+}
+
+
+
 }
